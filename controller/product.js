43c69f74376a247d5294855f9b03b37a36fd2572
alejg <alejandro.garza@revature.net>
2021-04-28 18:09:35 -0500

    product route updated/done

--- controller/product.js
+++ controller/product.js
@@ -1,37 +1,50 @@
+const express = require('express');
+const router = express.Router();
 const Product = require('../model/products');
+const session = require('express-session');
+
+router.use(session({ secret: 'secret', saveUninitialized: false, resave: false }));
 
 
 
-const addProduct = async (
-    product_name,
-    product_code,
-    description,
-    price,
-    rating,
-    manufacturer,
-    osType) => {
-    
+const addProduct = async (req, res) => {
+  const product_name = req.body.product_name;
+  const product_code = req.body.product_code;
+  const description = req.body.description;
+  const price = req.body.price;
+  const rating = req.body.rating;
+  const manufacturer = req.body.manufacturer;
+  const osType = req.body.osType;
+  try {
     const product = new Product({
-        product_name,
-        product_code,
-        description,
-        price,
-        rating,
-        manufacturer,
-        osType
+      product_name,
+      product_code,
+      description,
+      price,
+      rating,
+      manufacturer,
+      osType
     });
-    return product.save();
-  };
+    product.save();
+    return res.status(201).send(product);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+};
 
 
-  const findProduct = async (osType) => {
-    
-        const products = await Product.find({osType: osType})
-        return products;
-      };
+const findProduct = async (req, res) => {
+  try {
+    const osType = req.params.osType;
+       
+    const products = await Product.find({ osType: osType })
+  
+    return res.status(201).send(products);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+};
 
 
 
-
-
-  module.exports = { addProduct, findProduct};
+module.exports = { addProduct, findProduct};
--- routes/product.js
+++ routes/product.js
@@ -8,48 +8,14 @@ router.use(session({ secret: 'secret', saveUninitialized: false, resave: false }
 
 //add product
 
-router.post('/add', async (req, res) => {
-  try {
-    const product_name = req.body.product_name;
-    const product_code = req.body.product_code;
-    const description = req.body.description;
-    const price = req.body.price;
-    const rating = req.body.rating;
-    const manufacturer = req.body.manufacturer;
-    const osType = req.body.osType;
-       
-    const product = await productController.addProduct(
-        product_name,
-        product_code,
-        description,
-        price,
-        rating,
-        manufacturer,
-        osType);
-
-    return res.status(201).send(product);
-  } catch (error) {
-    res.status(400).send(error.message);
-  }
-});
+router.post('/add', productController.addProduct);
 
 
 // get phone by OS 
-router.get('/find/:osType', async (req, res) => {
-    try {
-      const osType = req.params.osType;
-         
-      const type  = await productController.findProduct(
-          osType);
-  
-      return res.status(201).send(type);
-    } catch (error) {
-      res.status(400).send(error.message);
-    }
-  });
+router.get('/find/:osType', productController.findProduct);
 
-
-  
+ 
+// delete droduct by product id
 
 
 module.exports = router;
